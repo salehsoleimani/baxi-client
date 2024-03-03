101289ac7448d3dd68ced9c48e149e7540804e1f
navasolaimanian <106958586+navasolaimanian@users.noreply.github.com>
2024-03-04 00:18:43 +0330

    adding profile page

--- src/components/map/Map.jsx
+++ src/components/map/Map.jsx
@@ -1,39 +1,62 @@
-import { useEffect, useRef } from "react"
-import { Map } from "@neshan-maps-platform/ol"
-import NeshanMap, { NeshanMapRef } from "@neshan-maps-platform/react-openlayers"
-import styles from './Map.module.css'
-import LocationBar from "./LocationBar"
-import ol from '@neshan-maps-platform/react-openlayers'
-
+import { useEffect, useRef, useState } from "react";
+import NeshanMap from "@neshan-maps-platform/react-openlayers";
+import LocationBar from "./LocationBar";
+import styles from './Map.module.css';
+import ol from '@neshan-maps-platform/react-openlayers';
+// import { Point } from 'ol/geom';
 
 const Maps = () => {
+    // const mapRef = useRef(null);
+    // const [marker, setMarker] = useState(null);
 
-    // const key = 'web.11c86d9ad4654f708f74425d9a297b9f';
+    // useEffect(() => {
+    //     const map = mapRef.current; // Directly access the map instance
 
-    // const attribution = new ol.control.Attribution({
-    //     collapsible: false,
-    // });
+    //     if (map) {
+    //         // Create a new marker feature
+    //         const markerFeature = new ol.Feature({
+    //             geometry: new ol.geom.Point(ol.proj.fromLonLat([51.4749824, 35.7665394])),
+    //         });
 
-    // const source = new ol.source.TileJSON({
-    //     url: `https://api.maptiler.com/maps/streets-v2/tiles.json?key=${key}`, // source URL
-    //     tileSize: 512,
-    //     crossOrigin: 'anonymous'
-    // });
+    //         // Create a marker style
+    //         const markerStyle = new ol.style.Style({
+    //             image: new ol.style.Icon({
+    //                 src: 'path/to/your/marker/icon.png',
+    //                 anchor: [0.5, 1],
+    //             }),
+    //         });
 
-    // const map = new ol.Map({
-    //     layers: [
-    //         new ol.layer.Tile({
-    //             source: source
-    //         })
-    //     ],
-    //     controls: ol.control.defaults.defaults({ attribution: false }).extend([attribution]),
-    //     target: 'map',
-    //     view: new ol.View({
-    //         constrainResolution: true,
-    //         center: ol.proj.fromLonLat([12.550343, 55.665957]), // starting position [lng, lat]
-    //         zoom: 10 // starting zoom
-    //     })
-    // });
+    //         markerFeature.setStyle(markerStyle);
+
+    //         // Add the marker to the map
+    //         const vectorSource = new ol.source.Vector({
+    //             features: [markerFeature],
+    //         });
+
+    //         const vectorLayer = new ol.layer.Vector({
+    //             source: vectorSource,
+    //         });
+
+    //         map.addLayer(vectorLayer);
+
+    //         setMarker(markerFeature);
+
+    //         // Update marker position when the map is moved
+    //         map.on('moveend', () => {
+    //             const center = map.getView().getCenter();
+    //             markerFeature.setGeometry(new ol.geom.Point(center));
+    //             console.log('Marker moved to:', center);
+    //         });
+
+    //         // Move marker to the clicked position
+    //         map.on('click', (event) => {
+    //             const coordinate = event.coordinate;
+    //             markerFeature.setGeometry(new ol.geom.Point(coordinate));
+    //             console.log('Marker set to:', coordinate);
+    //         });
+    //     }
+    // }, []);
+
     return (
         <div>
             <NeshanMap
@@ -41,11 +64,12 @@ const Maps = () => {
                 center={{ latitude: 35.7665394, longitude: 51.4749824 }}
                 zoom={14}
                 className={styles.map}
-                id= 'map'
-            ></NeshanMap>
+                id='map'
+                // ref={mapRef}
+            />
             <LocationBar />
         </div>
-    )
-}
+    );
+};
 
 export default Maps;
--- src/components/profile/UserInfo.jsx
+++ src/components/profile/UserInfo.jsx
@@ -1,10 +1,52 @@
-import rightArrow from '../../assets/icons/arrow-right.svg'
+import rightArrow from '../../assets/glass-icons/arrow-right.svg'
+import leftArrow from '../../assets/glass-icons/arrow-left.svg'
 import styles from './UsrInfo.module.css'
+import edit from '../../assets/glass-icons/edit-2.svg'
+import wallet from '../../assets/glass-icons/Frame 427320643.svg'
+import treap from '../../assets/glass-icons/Frame 427320644.svg'
+import address from '../../assets/glass-icons/Frame 427320645.svg'
 
 const UserInfo = () => {
     return (
-        <div>
-            <img src={rightArrow} />
+        <div className={styles.userInfo}>
+            <div>
+                <img src={rightArrow} className={styles.icon} />
+                <div className={styles.userEdit}>
+                    <div>
+                        <div className={`headline ${styles.user}`}>محمد صالح سلیمانی</div>
+                        <div className={`sub-headline`}>۰۹۳۶۶۶۱۸۲۴۳</div>
+                    </div>
+                    <img src={edit} />
+                </div>
+            </div>
+            <div>
+                <div className={styles.box}>
+                    <div className={styles.wallet}>
+                        <img src={wallet} />
+                        <div>
+                            <p className='sub-headline'>کیف پول</p>
+                            <p className='body'> موجودی  7,000  تومان</p>
+                        </div>
+                    </div>
+                    <img src={leftArrow} />
+                </div>
+                <div className={styles.box}>
+                    <div className={styles.wallet}>
+                        <img src={treap} />
+                        <div>
+                            <p className='sub-headline'>سفر های من</p>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.box}>
+                    <div className={styles.wallet}>
+                        <img src={address} />
+                        <div>
+                            <p className='sub-headline'>آدرس های منتخب</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
