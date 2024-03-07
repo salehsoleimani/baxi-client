f249b40270b38113605797d454ab394c8bb68ed2
Saleh <sabasipyh@gmail.com>
2024-03-07 12:05:23 +0330

    added: show markers on map

--- src/components/ui/SheetContent.jsx
+++ src/components/ui/SheetContent.jsx
@@ -2,12 +2,8 @@ import cx from 'classnames'
 import { forwardRef } from 'react'
 import styles from './SheetContent.module.css'
 
-type Props = {
-    children: React.ReactNode
-} & Omit<React.PropsWithoutRef<JSX.IntrinsicElements['div']>, 'children'>
-
-const SheetContent = forwardRef<HTMLDivElement, Props>(
-    ({ className, ...props }: Props, ref) => (
+const SheetContent = forwardRef(
+    ({ className, ...props }, ref) => (
         <div
             className={cx(
                 styles.spacing,
