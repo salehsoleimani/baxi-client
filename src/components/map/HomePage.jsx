3574d130089144bffe7ef2e1e1ed346dca50ebb0
Saleh <sabasipyh@gmail.com>
2024-03-07 14:18:46 +0330

    added: dest and org marker

--- src/components/map/HomePage.jsx
+++ src/components/map/HomePage.jsx
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef} from 'react';
 import styles from './HomePage.module.css'
-import {BottomSheet} from 'react-spring-bottom-sheet'
+import {BottomSheet} from "react-spring-bottom-sheet";
 import 'react-spring-bottom-sheet/dist/style.css'
 import {useState} from "react";
 import Button from "../ui/Button";
@@ -8,10 +8,17 @@ import Input from "../ui/Input";
 import search from "../../assets/icons/search-normal.svg";
 import location from "../../assets/icons/location.svg";
 import NeshanMap from "@neshan-maps-platform/react-openlayers"
+import * as layer from '@neshan-maps-platform/ol/layer'
+import * as source from '@neshan-maps-platform/ol/source'
+import {Collection, Feature} from "@neshan-maps-platform/ol";
+import {fromLonLat, toLonLat} from "@neshan-maps-platform/ol/proj";
+import {Point} from "@neshan-maps-platform/ol/geom";
+import {Fill, Icon, Stroke, Style} from "@neshan-maps-platform/ol/style";
 import SheetContent from "../ui/SheetContent.tsx";
+import orgMarker from "../../assets/icons/marker_org.svg"
+import {Translate} from "@neshan-maps-platform/ol/interaction";
 
-
-const Maps = () => {
+const HomePage = () => {
     const [open, setOpen] = useState(true);
     const neshanToken = 'web.11c86d9ad4654f708f74425d9a297b9f';
 
@@ -20,6 +27,64 @@ const Maps = () => {
     const onInit = (map) => {
         // map.setMapType("osm-bright")
         // map.switchTrafficLayer(true)
+
+        var marker = new Feature({
+            geometry: new Point(fromLonLat([48.5148, 34.7983])), name: 'userLocation',
+
+        });
+
+        marker.setStyle(new Style({
+            image: new Icon({
+                anchor: [.75, 1], scale: 2, src: orgMarker
+            })
+
+        }));
+
+
+        var vectorSource = new source.Vector({
+            features: [marker]
+        });
+
+        var vectorLayer = new layer.Vector({
+            source: vectorSource
+        });
+
+
+        var translate = new Translate({
+            features: new Collection([marker])
+        });
+
+
+        var translating = false;
+
+        map.on("singleclick", function (e) {
+            if (!translating) {
+
+                //do something
+
+            }
+        });
+
+
+        translate.on('translatestart', function (evt) {
+            translating = true;
+            console.log(evt.coordinate)
+        });
+
+        translate.on('translateend', function (evt) {
+
+            //do something else
+            console.log(evt.coordinate)
+
+            translating = false;
+        });
+
+        // translate.on('translateend', function(evt) { feature.getGeometry().setCoordinates(ol.proj.fromLonLat(ol.proj.toLonLat(evt.coordinate))) });
+
+        map.addLayer(vectorLayer);
+        map.addInteraction(translate);
+
+        console.log(map)
     }
 
     useEffect(() => {
@@ -34,7 +99,7 @@ const Maps = () => {
             className={styles.map}
             mapKey={neshanToken}
             defaultType="dreamy-gold"
-            center={{latitude:34.7983, longitude: 48.5148}}
+            center={{latitude: 34.7983, longitude: 48.5148}}
             onInit={onInit}
             zoom={16}
             traffic={true}
@@ -81,4 +146,4 @@ const Maps = () => {
     </div>
 };
 
-export default Maps;
+export default HomePage;
