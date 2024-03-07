12482813c3472358b1ce4c445dfbe0c50e85ef0b
Saleh <sabasipyh@gmail.com>
2024-03-07 23:50:05 +0330

    added: choosing org and dest

--- src/components/map/HomePage.jsx
+++ src/components/map/HomePage.jsx
@@ -25,21 +25,11 @@ const HomePage = () => {
     const [dest, setDest] = useState(null);
     const [clickCount, setClickCount] = useState(0);
     const [inputValue, setInputValue] = useState('');
-    const [buttonText, setButtonText] = useState('تایید مبدا');
+    const [buttonText, setButtonText] = useState('ثبت مبدا');
     const mapRef = useRef(null);
+    const [orgDrawn, setOrgDrawn] = useState(false);
 
     const onInit = (map) => {
-        const newMarker = new Feature({
-            geometry: new Point(fromLonLat([48.5148, 34.7983])),
-            name: 'userLocation',
-        });
-
-        newMarker.setStyle(new Style({
-            image: new Icon({
-                anchor: [.75, 1], scale: 2, src: orgMarker
-            })
-        }));
-
         const source = new Vector();
         const vector = new VectorLayer({
             source: source, style: new Style({
@@ -49,17 +39,7 @@ const HomePage = () => {
             })
         });
 
-        const destSource = new Vector();
-        const destVector = new VectorLayer({
-            source: destSource, style: new Style({
-                image: new Icon({
-                    anchor: [.75, 1], scale: 2, src: destMarker
-                }), fill: '#000000',
-            })
-        });
-
         map.addLayer(vector);
-        map.addLayer(destVector);
 
         const modify = new Modify({
             source: source, style: new Style({
@@ -69,15 +49,22 @@ const HomePage = () => {
             }), pixelTolerance: 40
         });
         map.addInteraction(modify);
-
-        const modifyDest = new Modify({
-            source: destSource, style: new Style({
-                image: new Icon({
-                    anchor: [.75, 1], scale: 2, src: destMarker
-                }),
-            }), pixelTolerance: 40
+        modify.on('modifyend', async function (event) {
+            if (event.features && event.features.getLength() > 0) {
+                const modifiedFeature = event.features.getArray()[0];
+                if (modifiedFeature) {
+                    const geometry = modifiedFeature.getGeometry();
+                    if (geometry) {
+                        const modifiedCoordinates = geometry.getCoordinates();
+                        setOrg(toLonLat(modifiedCoordinates));
+                        setOrgDrawn(true)
+                        const orgAddress = await fetchAddress(toLonLat(modifiedCoordinates));
+                        setInputValue(orgAddress);
+                    }
+                }
+            }
         });
-        map.addInteraction(modifyDest);
+
 
         function addInteractions() {
             const draw = new Draw({
@@ -89,41 +76,18 @@ const HomePage = () => {
             });
             draw.on('drawstart', function () {
             });
-            draw.on('drawend', function (event) {
-                const point = event.feature.getGeometry().getCoordinates()
-                const point_in_lang = toLonLat(point);
-                if (clickCount === 0) {
-                    setOrg(point_in_lang);
-                    map.removeInteraction(draw);
-                    const newDraw = new Draw({
-                        source: destSource, type: "Point", style: new Style({
-                            image: new Icon({
-                                anchor: [.75, 1], scale: 2, src: destMarker
-                            }),
-                        })
-                    });
-                    newDraw.on('drawstart', function () {
-                    });
-                    newDraw.on('drawend', function (event) {
-                        const point = event.feature.getGeometry().getCoordinates()
-                        const point_in_lang = toLonLat(point);
-                        setDest(point_in_lang);
-                        map.removeInteraction(newDraw);
-                    });
-                    map.addInteraction(newDraw);
-                    // setClickCount(1);
-                } else if (clickCount === 1) {
-                    setDest(event.feature.getGeometry().getCoordinates());
-                    map.removeInteraction(draw);
-                }
+            draw.on('drawend', async function (event) {
+                setOrg(toLonLat(event.feature.getGeometry().getCoordinates()));
+                map.removeInteraction(draw);
+                setOrgDrawn(true);
+                const orgAddress = await fetchAddress(toLonLat(event.feature.getGeometry().getCoordinates()));
+                setInputValue(orgAddress);
             });
 
             map.addInteraction(draw);
             const snap = new Snap({source: source});
             map.addInteraction(snap);
 
-            const destSnap = new Snap({source: destSource});
-            map.addInteraction(destSnap);
         }
 
         addInteractions();
@@ -131,20 +95,75 @@ const HomePage = () => {
 
     const handleClick = async () => {
         if (clickCount === 0) {
-            const orgAddress = await fetchAddress(org);
-            setInputValue(orgAddress);
-            setButtonText('انتخاب مقصد');
+            setButtonText('ثبت مقصد');
             setClickCount(1);
+
+            const destSource = new Vector();
+            const destVector = new VectorLayer({
+                source: destSource, style: new Style({
+                    image: new Icon({
+                        anchor: [.75, 1], scale: 2, src: destMarker
+                    }), fill: '#000000',
+                })
+            });
+
+            mapRef.current.map.addLayer(destVector);
+
+            const modifyDest = new Modify({
+                source: destSource, style: new Style({
+                    image: new Icon({
+                        anchor: [.75, 1], scale: 2, src: destMarker
+                    }),
+                }), pixelTolerance: 40
+            });
+            mapRef.current.map.addInteraction(modifyDest);
+            modifyDest.on('modifyend', async function (event) {
+                if (event.features && event.features.getLength() > 0) {
+                    const modifiedFeature = event.features.getArray()[0];
+                    if (modifiedFeature) {
+                        const geometry = modifiedFeature.getGeometry();
+                        if (geometry) {
+                            const modifiedCoordinates = geometry.getCoordinates();
+                            setDest(toLonLat(modifiedCoordinates));
+                            const destAddress = await fetchAddress(toLonLat(modifiedCoordinates));
+                            setInputValue(destAddress);
+                        }
+                    }
+                }
+            });
+
+            const drawDest = new Draw({
+                source: destSource, type: "Point", style: new Style({
+                    image: new Icon({
+                        anchor: [.75, 1], scale: 2, src: destMarker
+                    }),
+                })
+            });
+            drawDest.on('drawend', async function (event) {
+                const destCoordinates = toLonLat(event.feature.getGeometry().getCoordinates());
+                setDest(destCoordinates);
+                const destAddress = await fetchAddress(destCoordinates);
+                setInputValue(destAddress);
+                mapRef.current.map.removeInteraction(drawDest)
+            });
+
+            mapRef.current.map.addInteraction(drawDest);
+
+
+            const destSnap = new Snap({source: destSource});
+            mapRef.current.map.addInteraction(destSnap);
+
+
         } else if (clickCount === 1) {
             const destAddress = await fetchAddress(dest);
             setInputValue(destAddress);
             setButtonText('');
             setClickCount(0);
+            setOrgDrawn(false);
         }
     };
 
     const fetchAddress = async (coordinates) => {
-        console.log(coordinates)
         const serviceToken = "service.56044bc8cffe4511b9742d9fbaa00da9"
         const url = `https://api.neshan.org/v1/reverse?lat=${coordinates[1]}&lng=${coordinates[0]}`;
         const response = await fetch(url, {
@@ -162,49 +181,48 @@ const HomePage = () => {
         }
     }, []);
 
-    return (
-        <div>
-            <NeshanMap
-                className={styles.map}
-                mapKey={neshanToken}
-                defaultType="dreamy-gold"
-                center={{latitude: 34.7983, longitude: 48.5148}}
-                onInit={onInit}
-                zoom={16}
-                traffic={true}
-                poi={false}
-            ></NeshanMap>
-            <BottomSheet
-                open={open}
-                blocking={false}
-                expandOnContentDrag={true}
-                snapPoints={({minHeight}) => minHeight}
-            >
-                <SheetContent className={styles.LocationBar}>
-                    <Input
-                        placeholder="جستجوی..."
-                        className={styles.inputContainer}
-                        type="search"
-                        defaultValue={inputValue}
-                    >
-                        <img src={search} alt="search"/>
-                        <img src={location} alt="location"/>
-                    </Input>
-                    <div className={styles.savedLocations}>
-                        <div className={styles.savedLocation}>
-                            <span>خونه</span>
-                        </div>
-                        <div className={styles.savedLocation}>
-                            <span>خونه</span>
-                        </div>
+    return (<div>
+        <NeshanMap
+            className={styles.map}
+            mapKey={neshanToken}
+            defaultType="dreamy-gold"
+            center={{latitude: 34.7983, longitude: 48.5148}}
+            onInit={onInit}
+            zoom={16}
+            ref={mapRef}
+            traffic={true}
+            poi={false}
+        ></NeshanMap>
+        <BottomSheet
+            open={open}
+            blocking={false}
+            expandOnContentDrag={true}
+            snapPoints={({minHeight}) => minHeight}
+        >
+            <SheetContent className={styles.LocationBar}>
+                <Input
+                    placeholder="جستجوی..."
+                    className={styles.inputContainer}
+                    type="search"
+                    defaultValue={inputValue}
+                >
+                    <img src={search} alt="search"/>
+                    <img src={location} alt="location"/>
+                </Input>
+                <div className={styles.savedLocations}>
+                    <div className={styles.savedLocation}>
+                        <span>خانه</span>
                     </div>
-                    {buttonText && (<Button type="filled" className={styles.bottomSheetButton} onClick={handleClick}>
-                        <span className="button">{buttonText}</span>
-                    </Button>)}
-                </SheetContent>
-            </BottomSheet>
-        </div>
-    );
+                    <div className={styles.savedLocation}>
+                        <span>خانه</span>
+                    </div>
+                </div>
+                {orgDrawn && (<Button type="filled" className={styles.bottomSheetButton} onClick={handleClick}>
+                    <span className="button">{buttonText}</span>
+                </Button>)}
+            </SheetContent>
+        </BottomSheet>
+    </div>);
 };
 
 export default HomePage;
