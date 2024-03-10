a0a0b6f65eed13201af9676b2e52e7ca39ad7f78
navasolaimanian <106958586+navasolaimanian@users.noreply.github.com>
2024-03-10 12:41:25 +0330

    add address page

--- src/components/address/FirstAddress.jsx
+++ src/components/address/FirstAddress.jsx
@@ -1,21 +1,25 @@
 import styles from "./FirstAddress.module.css";
-// import rightArrow from "../../assets/glass-icons/arrow-right.svg";
-// import addImg from "../../assets/icons/address_empty_state.svg";
-// import add from "../../assets/icons/add.svg";
+import rightArrow from "../../assets/icons/arrow-right.svg";
+import addImg from "../../assets/img/addImg.png";
+import add from "../../assets/icons/add.svg";
+import Button from "../ui/Button";
 
 const FirstAddress = () => {
   return (
     <div className={styles.firstAddress}>
-      {/* <img src={rightArrow} className={styles.icon} />
+      <img src={rightArrow} className={styles.icon} />
       <p className={`title2`}>آدرس های منتخب</p>
       <div className={styles.addImg}>
         <img src={addImg} />
-        <div className="body">هنوز آدرس منتخبی ذخیره نکرده اید</div>
+        <div className="sub-headline">هنوز آدرس منتخبی ذخیره نکرده اید</div>
       </div>
-      <button className={`button2 ${styles.bottom}`}>
-        <img src={add} />
+      {/* <button className={`button2 ${styles.bottom}`}>
         <span>ذخیره آدرس جدید</span>
       </button> */}
+      <Button type="border" className={styles.button}>
+        <img src={add} />
+        <span>ذخیره آدرس جدید</span>
+      </Button>
     </div>
   );
 };
